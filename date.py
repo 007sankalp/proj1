f5f3d5ee4a900f5c84ed96b4dcc6a2ff6d4c6161
007sankalp <sankalp.saxena007@gmail.com>
2017-07-29 17:26:38 +0530

    student module complete

---
+++ date.py
@@ -0,0 +1,11 @@
+#import the module date.py
+
+print("good morning all")
+
+print("good afternoon to all")
+
+#call the fucntion from date.py and print the date
+
+# create student object and call the method
+
+#create one more student object and call the method
