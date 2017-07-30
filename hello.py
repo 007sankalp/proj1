ecc0fa538e0210b4e29fad52cd2aa226af38b196
007sankalp <sankalp.saxena007@gmail.com>
2017-07-30 09:52:25 +0530

    moved the printing to a variable

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print ("Good Morning to All")
+
+
+msg="Good evemning  to all"
+
+print (msg)
+
+
+#call the fucntion from date.py and print the date
+
+# create student object and call the method
+
+#create one more student object and call the method
