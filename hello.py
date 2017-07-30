dbc9345f2cc1ece2c93a847e6f404018f57ed703
007sankalp <sankalp.saxena007@gmail.com>
2017-07-30 09:57:56 +0530

    bug fixed

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print ("Good Morning to All")
+
+
+msg="Good evenning  to all"
+
+print (msg)
+
+print ("good night to all")
+
+
+#call the fucntion from date.py and print the date
+
+# create student object and call the method
+
+#create one more student object and call the method
